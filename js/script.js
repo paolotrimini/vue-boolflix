4b1d97ea4427b187a4b1ceb1985d8b7da4da94a2
paolotrimini <paolo.trimini@gmail.com>
2021-04-20 12:19:21 +0200

    film + serie

--- js/script.js
+++ js/script.js
@@ -1,7 +1,60 @@
+    function initVue() {
 
-    function initVue(){
+        var app = new Vue({
 
-    }
+            el: '#app',
+            data: {
+                search: 'matrix',
+                movies: []
+
+            },
+
+           methods: {
+
+               searchMovie: function() {
+
+                   const params = {
+                       params: {
+                           'api_key': '7ff024206457c3360e440b86eb061a79',
+                           'query': this.search,
+                           'language': 'it'
+                       }
+                   };
+
+                   axios.get('https://api.themoviedb.org/3/search/movie',params)
+                       .then(data => {
+                           this.movies = data.data.results;
+                           console.log(this.movies);
+                           });
+
+                   axios.get('https://api.themoviedb.org/3/search/tv',params)
+                       .then(data => {
+                           this.series = data.data.results;
+                           console.log(this.series);
+                       });
+
+               }, // fine searchMovie()
+
+               isFlaggable: function(lang){
+                   if(lang == 'en' || lang == 'it')
+                       return true;
+                   return false;
+               },
+
+               getFlag: function(lang){
+                   switch(lang) {
+                       case 'en': return 'en.png';
+                       case 'it': return 'it.png';
+                   }
+                   return lang;
+               }
+
+           } // fine methods
+
+        }) // fine newVue
+
+
+        }
 
     function init() {
         initVue();
